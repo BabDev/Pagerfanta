5028a9e2fe2bf86294ed9cb2cdb44aec936c5452
pablodip <pablodip@gmail.com>
2011-05-06 16:11:59 +0100

    added DoctrineCollectionAdapter

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,6 +8,7 @@ $classLoader = new UniversalClassLoader();
 $classLoader->registerNamespaces(array(
     'Pagerfanta'           => __DIR__.'/../src',
     'Mandango'             => __DIR__.'/../vendor/mandango/src',
+    'Doctrine\Common'      => __DIR__.'/../vendor/doctrine-common/lib',
     'Doctrine\MongoDB'     => __DIR__.'/../vendor/doctrine-mongodb/lib',
     'Doctrine\ODM\MongoDB' => __DIR__.'/../vendor/doctrine-mongodb-odm/lib',
 ));
