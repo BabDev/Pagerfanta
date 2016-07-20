e4e13be0b441c31ec12260e8a22b9f7b26595ba9
Raphael Yan <raphael@donkeycode.com>
2016-07-20 09:54:37 +0200

    add adaptater for propel2 (no limit(0))

---
+++ src/Pagerfanta/Adapter/Propel2Adapter.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the Pagerfanta package.
+ *
+ * (c) Pablo Díez <pablodip@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pagerfanta\Adapter;
+
+/**
+ * Propel2Adapter.
+ *
+ * @author Raphael YAN <raphael.yan89@gmail.com>
+ */
+class Propel2Adapter implements AdapterInterface
+{
+    private $query;
+
+    /**
+     * Constructor.
+     *
+     * @param \ModelCriteria $query
+     */
+    public function __construct($query)
+    {
+        $this->query = $query;
+    }
+
+    /**
+     * Returns the query.
+     *
+     * @return \ModelCriteria
+     */
+    public function getQuery()
+    {
+        return $this->query;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNbResults()
+    {
+        $q = clone $this->getQuery();
+
+        $q->offset(0);
+
+        return $q->count();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSlice($offset, $length)
+    {
+        $q = clone $this->getQuery();
+
+        $q->limit($length);
+        $q->offset($offset);
+
+        return $q->find();
+    }
+}
