84b57c58a39f266c9fdc9521d98dc5ead7d822ef
Surgie Finesse <finesserus@gmail.com>
2017-10-08 20:23:07 +1000

    Create the adapter

---
+++ src/Pagerfanta/Adapter/ConcatenationAdapter.php
@@ -0,0 +1,130 @@
+<?php
+
+/*
+ * This file is part of the Pagerfanta package.
+ *
+ * (c) Pablo Díez <pablodip@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pagerfanta\Adapter;
+
+use Pagerfanta\Exception\InvalidArgumentException;
+
+/**
+ * Adapter that concatenates other adapters.
+ *
+ * @author Surgie Finesse <finesserus@gmail.com>
+ */
+class ConcatenationAdapter implements AdapterInterface
+{
+    /**
+     * @var AdapterInterface[] The list of adapters
+     */
+    protected $adapters;
+
+    /**
+     * @var int[]|null Cache of the numbers of results of the adapters. The indexes correspond the indexes of the
+     * $adapters property.
+     */
+    protected $adaptersNbResultsCache;
+
+    /**
+     * @param AdapterInterface[] $adapters
+     */
+    public function __construct(array $adapters)
+    {
+        foreach ($adapters as $index => $adapter) {
+            if (!($adapter instanceof AdapterInterface)) {
+                throw new InvalidArgumentException(
+                    'Argument $adapters['.$index.'] expected to be a Pagerfanta\Adapter\AdapterInterface instance'
+                        . ', a '.(is_object($adapter) ? get_class($adapter).' instance' : gettype($adapter)).' given'
+                );
+            }
+        }
+
+        $this->adapters = $adapters;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getNbResults()
+    {
+        if (!isset($this->adaptersNbResultsCache)) {
+            $this->refreshAdaptersNbResults();
+        }
+
+        return array_sum($this->adaptersNbResultsCache);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getSlice($offset, $length)
+    {
+        if (!isset($this->adaptersNbResultsCache)) {
+            $this->refreshAdaptersNbResults();
+        }
+
+        $slice = array();
+        $previousAdaptersNbResultsSum = 0;
+        $searchFirstIndex = $offset;
+        $searchLastIndex  = $offset + $length - 1;
+
+        foreach ($this->adapters as $index => $adapter) {
+            $adapterNbResults  = $this->adaptersNbResultsCache[$index];
+            $adapterFirstIndex = $previousAdaptersNbResultsSum;
+            $adapterLastIndex  = $adapterFirstIndex + $adapterNbResults - 1;
+
+            $previousAdaptersNbResultsSum += $adapterNbResults;
+
+            // The adapter is below the requested slice range — skip it
+            if ($adapterLastIndex < $searchFirstIndex) {
+                continue;
+            }
+
+            // The adapter is above the requested slice range — finish the gathering
+            if ($adapterFirstIndex > $searchLastIndex) {
+                break;
+            }
+
+            // Else the adapter range definitely intersects with the requested range
+            $fetchOffset = $searchFirstIndex - $adapterFirstIndex;
+            $fetchLength = $length;
+
+            // The requested range peeps above the bottom edge of the adapter range
+            if ($fetchOffset < 0) {
+                $fetchLength += $fetchOffset;
+                $fetchOffset = 0;
+            }
+
+            // The requested range peeps above the top edge of the adapter range
+            if ($fetchOffset + $fetchLength > $adapterNbResults) {
+                $fetchLength = $adapterNbResults - $fetchOffset;
+            }
+
+            // Getting the subslice from the adapter and adding it to the result slice
+            $fetchSlice = $adapter->getSlice($fetchOffset, $fetchLength);
+            $slice = array_merge($slice, $fetchSlice);
+        }
+
+        return $slice;
+    }
+
+    /**
+     * Refreshes the cache of the numbers of results of the adapters.
+     */
+    protected function refreshAdaptersNbResults()
+    {
+        if (!isset($this->adaptersNbResultsCache)) {
+            $this->adaptersNbResultsCache = array();
+        }
+
+        foreach ($this->adapters as $index => $adapter) {
+            $this->adaptersNbResultsCache[$index] = $adapter->getNbResults();
+        }
+    }
+}
