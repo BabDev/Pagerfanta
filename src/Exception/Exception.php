45195c69c63f3543dab6aeda65158c9a395c2953
Michael Babker <michael.babker@gmail.com>
2020-06-08 09:18:25 -0500

    Add doc block

--- src/Exception/Exception.php
+++ src/Exception/Exception.php
@@ -4,6 +4,9 @@ namespace Pagerfanta\Exception;
 
 trigger_deprecation('babdev/pagerfanta', '2.2', 'The "%s" interface is deprecated and will be removed in 3.0, exceptions should implement "%s" instead.', Exception::class, PagerfantaException::class);
 
+/**
+ * @deprecated to be removed in 3.0, exceptions should implement `Pagerfanta\Exception\PagerfantaException` instead
+ */
 interface Exception extends PagerfantaException
 {
 }
