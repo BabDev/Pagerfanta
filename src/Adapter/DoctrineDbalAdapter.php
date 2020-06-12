1f128d0693c6dcf6016ec3e1c78648ad9eb1edb8
Michael Babker <michael.babker@gmail.com>
2020-06-11 19:55:37 -0500

    Fix deprecation messages

--- src/Adapter/DoctrineDbalAdapter.php
+++ src/Adapter/DoctrineDbalAdapter.php
@@ -4,7 +4,7 @@ namespace Pagerfanta\Adapter;
 
 use Pagerfanta\DoctrineDBAL\DBALQueryAdapter;
 
-trigger_deprecation('pagerfanta/pagerfanta', '2.4', 'The "%s" class is deprecated and will be removed in 3.0. Use the "%s" class from the "pagerfanta/doctrine-collections-adapter" package instead.', DoctrineDbalAdapter::class, DBALQueryAdapter::class);
+trigger_deprecation('pagerfanta/pagerfanta', '2.4', 'The "%s" class is deprecated and will be removed in 3.0. Use the "%s" class from the "pagerfanta/doctrine-dbal-adapter" package instead.', DoctrineDbalAdapter::class, DBALQueryAdapter::class);
 
 /**
  * Adapter which calculates pagination from a Doctrine DBAL QueryBuilder.
--- src/Adapter/DoctrineDbalSingleTableAdapter.php
+++ src/Adapter/DoctrineDbalSingleTableAdapter.php
@@ -4,7 +4,7 @@ namespace Pagerfanta\Adapter;
 
 use Pagerfanta\DoctrineDBAL\DBALSingleTableQueryAdapter;
 
-trigger_deprecation('pagerfanta/pagerfanta', '2.4', 'The "%s" class is deprecated and will be removed in 3.0. Use the "%s" class from the "pagerfanta/doctrine-collections-adapter" package instead.', DoctrineDbalSingleTableAdapter::class, DBALSingleTableQueryAdapter::class);
+trigger_deprecation('pagerfanta/pagerfanta', '2.4', 'The "%s" class is deprecated and will be removed in 3.0. Use the "%s" class from the "pagerfanta/doctrine-dbal-adapter" package instead.', DoctrineDbalSingleTableAdapter::class, DBALSingleTableQueryAdapter::class);
 
 /**
  * Extended Doctrine DBAL adapter which assists in building the count query modifier for a SELECT query on a single table.
