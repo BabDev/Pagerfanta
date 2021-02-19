37c427900c5d4a07721d4de72efdc57e46777633
Michael Babker <michael.babker@gmail.com>
2021-02-18 18:40:11 -0600

    Update signatures and add strict types

--- lib/Core/View/TwitterBootstrap5View.php
+++ lib/Core/View/TwitterBootstrap5View.php
@@ -1,17 +1,18 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace Pagerfanta\View;
 
+use Pagerfanta\View\Template\TemplateInterface;
 use Pagerfanta\View\Template\TwitterBootstrap5Template;
 
 class TwitterBootstrap5View extends TwitterBootstrapView
 {
-    protected function createDefaultTemplate()
+    protected function createDefaultTemplate(): TemplateInterface
     {
         return new TwitterBootstrap5Template();
     }
 
-    public function getName()
+    public function getName(): string
     {
         return 'twitter_bootstrap5';
     }
