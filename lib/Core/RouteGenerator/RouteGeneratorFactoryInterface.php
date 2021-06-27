0a9a6f0681e26692c681f74e04ee5af79bd4638b
Michael Babker <michael.babker@gmail.com>
2021-06-27 14:58:20 -0500

    Document that route generator factories can throw exceptions (matches the existing implementation from the Symfony bundle)

--- lib/Core/RouteGenerator/RouteGeneratorFactoryInterface.php
+++ lib/Core/RouteGenerator/RouteGeneratorFactoryInterface.php
@@ -2,7 +2,12 @@
 
 namespace Pagerfanta\RouteGenerator;
 
+use Pagerfanta\Exception\RuntimeException;
+
 interface RouteGeneratorFactoryInterface
 {
+    /**
+     * @throws RuntimeException if the route generator cannot be created
+     */
     public function create(array $options = []): RouteGeneratorInterface;
 }
