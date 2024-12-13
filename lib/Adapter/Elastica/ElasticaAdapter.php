2c115945661ab121269459b2c900ed153ef2b919
Christophe Coevoet <stof@notk.org>
2024-12-13 15:57:28 +0100

    Add missing iterable value types (#60)

--- lib/Adapter/Elastica/ElasticaAdapter.php
+++ lib/Adapter/Elastica/ElasticaAdapter.php
@@ -24,6 +24,7 @@ class ElasticaAdapter implements AdapterInterface
     private ?ResultSet $resultSet = null;
 
     /**
+     * @param array<string, mixed> $options
      * @param int|null $maxResults Limit the number of totalHits returned by ElasticSearch; see https://github.com/whiteoctober/Pagerfanta/pull/213#issue-87631892
      *
      * @throws NotValidResultCountException if the maximum number of results is less than zero
