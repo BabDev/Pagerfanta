79f3ec4f064250644685ff0eb740b5e8fff74757
Michael Babker <michael.babker@gmail.com>
2025-03-25 23:35:37 -0400

    Rector and CS fixer config updates

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,15 +4,15 @@ return (new PhpCsFixer\Config())
     ->setRules([
         '@Symfony' => true,
         '@Symfony:risky' => true,
-        '@PHP81Migration' => true,
-        '@PHP80Migration:risky' => true,
+        '@PHP82Migration' => true,
+        '@PHP82Migration:risky' => true,
         '@PHPUnit100Migration:risky' => true,
         'array_syntax' => ['syntax' => 'short'],
         'blank_line_after_opening_tag' => false,
         'fopen_flags' => false,
         'linebreak_after_opening_tag' => false,
         'no_superfluous_phpdoc_tags' => ['remove_inheritdoc' => true],
-        'nullable_type_declaration_for_default_null_value' => ['use_nullable_type_declaration' => true],
+        'nullable_type_declaration_for_default_null_value' => true,
         'single_line_empty_body' => true,
     ])
     ->setRiskyAllowed(true)
