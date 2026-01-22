cc9f93ed205f2c96d4c593d2dc0b0434d37a5b50
Michael Babker <michael.babker@gmail.com>
2026-01-22 08:51:30 -0500

    Fix tests

--- rector.php
+++ rector.php
@@ -2,6 +2,7 @@
 
 use Rector\Config\RectorConfig;
 use Rector\PHPUnit\CodeQuality\Rector\Class_\NarrowUnusedSetUpDefinedPropertyRector;
+use Rector\Privatization\Rector\Class_\FinalizeTestCaseClassRector;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -14,6 +15,10 @@ return RectorConfig::configure()
 
         NarrowUnusedSetUpDefinedPropertyRector::class => [
             __DIR__.'/lib/Twig/Tests/View/TwigViewIntegrationTest.php', // Tries to inline the route generator factory which is used in the mocked runtime loader
+        ],
+
+        FinalizeTestCaseClassRector::class => [
+            __DIR__.'/lib/Core/Tests/View/TwitterBootstrapViewTest.php', // Test case class is extended by other Bootstrap variations
         ],
     ])
     ->withImportNames(importShortClasses: false)
